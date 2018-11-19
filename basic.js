a82fc0e75f123777392e71adbdc5529898738b54
Ali <HasanAliKaraca@users.noreply.github.com>
2018-11-19 17:32:43 +0300

    turkish image added

--- basic.js
+++ basic.js
@@ -1,15 +1,15 @@
 var Tesseract = require('tesseract.js')
-var image = "cosmic.png";
+var image = "turkishData.png";
 
 
 Tesseract.recognize(image, {lang:'tur'})
   .then(data => {
-    console.log('then\n', data.text)
+    console.log('DATA:\n\n', data.text)
   })
   .catch(err => {
-    console.log('catch\n', err);
+    console.log('catch:\n', err);
   })
   .finally(e => {
-    console.log('finally\n');
+    // console.log('finally:\n');
     process.exit();
   });
